a5db166effe1db6d1f1bcfd989eda7c0950dd94d
BMJ <BernieMJ1012@gmail.com>
2021-02-27 00:49:11 +0800

    change bg color

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,17 @@ function Footer() {
     <footer className="footer">
       <div className="container">
         <span style={{ fontSize: "14px" }}>
+          @Pasteright 2021{" "}
+          <a
+            href="https://github.com/BMJLearnToHelloWorld"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            BMJ
+          </a>
+        </span>
+        <span>&nbsp;&nbsp;</span>
+        <span style={{ fontSize: "14px" }}>
           Powered by{" "}
           <a
             href="https://github.com/facebook/create-react-app"
